0e2eb62bd96de6a8a84679b2d245ebfa28c01c60
Chris Pinkham <cpinkham@mythtv.org>
2016-01-27 21:56:10 -0800

    Update help text.

--- help/config.php
+++ help/config.php
@@ -7,5 +7,7 @@ the top of the page.<br>
 <br>
 For each button, you can configure the description, color, and the name of
 the script to be run when the button is clicked.  The script output will
-not be displayed.<br>
+not be displayed.  A button will change color signifying when it has
+been pressed, but no other feedback will be given as to the success or
+failure of the executed script.<br>
 <br>
